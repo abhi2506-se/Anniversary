178246b3cf0cf33dc571135fa1e61fcdcef2e2e7
Abhishek Singh <abhisheksingh89208@gmail.com>
2026-04-29 12:14:04 +0530

    Add files via upload

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="py-6 text-center text-sm text-pink-200/70">
+      Developed with love by Abhishek Singh
+    </footer>
+  );
+}
